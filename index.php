9f695d461ee3ca520542ee90b4e00edb5892b253
Dimi Antoni Vargas <antonivargas@gmail.com>
2015-08-14 17:08:56 -0300

    foca no código
    foca no código

--- index.php
+++ index.php
@@ -1,12 +1,21 @@
 <?php 
+# focanocodigo
 /*
- * A Design by W3layouts
-Author: W3layouts
-Author URL: http://w3layouts.com
-License: Creative Commons Attribution 3.0 Unported
-License URL: http://creativecommons.org/licenses/by/3.0/
- *
- */
+**********************************************		  
+*		        .---.						                 *
+*		       /o   o\						               *	
+*		    __(=  "  =)__ 					             *
+*		     //\'-=-'/\\ 					               *
+*		        )   (_   					               *	
+*		       /      `"=-._                     *
+*		      /       \     ``"=.                *
+*		     /  /   \  \         `=..--.         *
+*		 ___/  /     \  \___      _,  , `\       * 
+*		`-----' `""""`'-----``"""`  \  \_/       *
+*		                             `-`         *
+**********************************************
+*/
+
 include "app/config.php";
 include "app/detect.php";
 
